ec94d009fc61c16dc1ade5a62f10de92ae01fb33
Isiteketo Mutau <mutau85@gmail.com>
2025-09-25 00:14:55 +0200

    Accept current changes in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-<<<<<<< HEAD
 import { Playfair_Display, Jost, Inter, Outfit } from "next/font/google";
-=======
-import {
-  Playfair_Display,
-  Jost,
-  Poppins,
-  Roboto,
-  Inter,
-} from "next/font/google";
->>>>>>> e662db40e2c772e8f8b4e5a7be1cec13ace7689f
 import "./globals.css";
 import Navigation from "@/components/Nav";
-import Footer from "@/components/Footer";
+import { Footer } from "@/components/Footer";
 
 const jost = Jost({
   subsets: ["latin"],
@@ -21,7 +11,6 @@ const jost = Jost({
   display: "swap",
 });
 
-<<<<<<< HEAD
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -39,44 +28,12 @@ const playfair = Playfair_Display({
   weight: ["400", "500", "600", "700", "800", "900"],
   variable: "--font-playfair",
   display: "swap",
-=======
-const playfair = Playfair_Display({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-  variable: "--font-playfair",
-  display: "swap",
-});
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800"],
-  variable: "--font-poppins",
-  display: "swap",
-});
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700", "900"],
-  variable: "--font-roboto",
-  display: "swap",
-});
-
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-  variable: "--font-inter",
-  display: "swap",
->>>>>>> e662db40e2c772e8f8b4e5a7be1cec13ace7689f
 });
 
 export const metadata: Metadata = {
   title: "Barotse Highway Limited",
   description:
-<<<<<<< HEAD
     "Barotse Highway Limited - Connecting Zambia's Rich mineral resources to the world",
-=======
-    "Barotse Highway Limited - Connecting Zambia's Mineral Wealth to International Markets",
->>>>>>> e662db40e2c772e8f8b4e5a7be1cec13ace7689f
 };
 
 export default function RootLayout({
@@ -87,11 +44,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-<<<<<<< HEAD
         className={`${playfair.variable} ${jost.variable} ${inter.variable} ${outfit.variable} antialiased`}
-=======
-        className={`${playfair.variable} ${jost.variable} ${poppins.variable} ${roboto.variable} ${inter.variable} antialiased`}
->>>>>>> e662db40e2c772e8f8b4e5a7be1cec13ace7689f
       >
         <Navigation />
         {children}
