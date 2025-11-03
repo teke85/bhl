72995b2e62652678fa5030a5f157abbd287af653
Isiteketo Mutau <mutau85@gmail.com>
2025-11-03 12:27:59 +0200

    increased height for logo in footer section

--- frontend/components/FooterUpdated.tsx
+++ frontend/components/FooterUpdated.tsx
@@ -39,13 +39,13 @@ export const Footer = () => {
           {/* Brand Section */}
           <div className="lg:col-span-4">
             <Link href="/" className="flex items-center gap-3 mb-6">
-              <div className="relative w-22 h-22 rounded-full overflow-hidden">
+              <div className="relative w-22 h-36 rounded-full overflow-hidden">
                 <Image
                   src="https://res.cloudinary.com/dpeg7wc34/image/upload/f_auto,q_70,w_300,c_limit/v1761708103/westerncorridor_logo_bih0jh.png"
                   alt="Western Corridor logo"
                   width={200}
                   height={200}
-                  className="h-30 w-30 md:h-35 md:w-35 object-contain drop-shadow-lg"
+                  className="h-30 w-30 md:h-46 md:w-46 object-contain drop-shadow-lg"
                   priority
                   quality={70}
                 />
